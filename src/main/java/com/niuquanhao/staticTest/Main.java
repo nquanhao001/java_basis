da626d3dde0fcbd3df2c45f1a6a1892cd6aa3fd9
nquanhao001 <407361943@qq.com>
2016-07-15 14:02:28 +0800

    提交代码

---
+++ src/main/java/com/niuquanhao/staticTest/Main.java
@@ -0,0 +1,14 @@
+package com.niuquanhao.staticTest;
+
+/**
+ * Created by niuquanhao on 16/7/7.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        TestUtil.get();
+        TestUtil.get();
+        TestUtil.get();
+        TestUtil.get();
+    }
+}
---
+++ src/main/java/com/niuquanhao/staticTest/TestUtil.java
@@ -0,0 +1,15 @@
+package com.niuquanhao.staticTest;
+
+/**
+ * Created by niuquanhao on 16/7/7.
+ */
+public class TestUtil {
+
+    static {
+        System.out.println("ddddddddd");
+    }
+
+    public static void get(){
+        System.out.println("fffff");
+    }
+}
